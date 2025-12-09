22316d7c83d6e83ce3ecddbe190855234ad56bb5
jasmine-shebl <jasmeneshebl@gmail.com>
2025-12-09 19:24:36 +0300

    creating a client side socket

--- client.py
+++ client.py
@@ -1,6 +1,16 @@
 import socket
 import threading
-import requests
 import json
-import sys 
-import time
+SERVER_ADDR=("127.0.0.1",50555)
+
+# function to start the client
+def start_client():
+     client_socket = socket.socket(family=socket.AF_INET, type= socket.SOCK_STREAM)#create socket
+
+     #connect to the server
+     client_socket.connect(SERVER_ADDR)
+     print(f"connected to server {SERVER_ADDR}")
+
+     #sent the username to the server
+     username=input("Enter your username:")
+     client_socket.sendall(username.encode("utf-8"))
