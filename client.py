c81a55042527c5ab43288bfd601d7f0d84f24e24
jasmine-shebl <jasmeneshebl@gmail.com>
2025-12-13 21:52:33 +0300

    receiving functions & a part of displaying functions

--- client.py
+++ client.py
@@ -23,20 +23,39 @@ def main_menu():
 def headlines_menu():
      print("\t \t \t Headlines Menu")
      print("1.1- Search for keywords ")
-     print("1.2- Search by country ") 
-     print("1.3- Search by category ")
+     print("1.2- Search by category ") 
+     print("1.3- Search by country ")
      print("1.4- List all new headlines ")
      print("1.5- Back to the main menu ")
 
 # display the submenu of 2- List of sources
 def sources_menu():
      print("\t \t \t Sources Menu")
-     print("2.1- Search by country ")
-     print("2.2- Search by language ") 
-     print("2.3- Search by category ")
+     print("2.1- Search by category ")
+     print("2.2- Search by country ") 
+     print("2.3- Search by language ")
      print("2.4- List all ")
      print("2.5- Back to the main menu ")
-  
+
+"""""""""""""""""""""""""""receiving functions"""""""""""""""""""""""""""
+
+#receive a text message from the server
+def recv_text(socket):
+    return socket.recv(BUF_SIZE).decoode("utf-8",errors="replace").strip()
+
+#receive json data from the server and convert it to object understood in python
+def recv_json(socket): 
+     data= socket.recv(BUF_SIZE).decode("utf-8",errors="replace")
+     return json.loads(data)
+
+"""""""""""""""""""""""""""displaying functions"""""""""""""""""""""""""""
+
+#display list of headlines
+def display_headlines_list(item):
+     print("\t \t \t Headlines list")
+     for i, item in enumerate(item):
+          
+
 # function to start the client
 def start_client():
      client_socket = socket.socket(family=socket.AF_INET, type= socket.SOCK_STREAM)#create socket
